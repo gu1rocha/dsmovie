e8dbc878adb732bbea0ed4a44f7933ccc026a2d6
Guilherme Rocha <guirocha12395@gmail.com>
2022-01-15 13:09:49 -0300

    Corrigindo commit Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/ScoreController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsmovie.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,11 @@ import com.devsuperior.dsmovie.services.ScoreService;
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
-
+	
+	@Autowired
 	private ScoreService service;
 	
-	@PutMapping(value = "/{id}")
+	@PutMapping
 	public MovieDTO saveScore(@RequestBody ScoreDTO dto){
 		MovieDTO movieDTO = service.saveScore(dto);
 		return movieDTO;
